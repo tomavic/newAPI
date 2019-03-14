a2b5726866e42ae6c9100f4a8cf138cba31cca71
tomavic <hbasheer@live.com>
2019-03-14 18:21:33 +0200

    hamada

--- startup/db.js
+++ startup/db.js
@@ -1,16 +1,9 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
-const username = 'boy';
-const password = 'ssLTBCiuQcgskAHu';
 
-const LiveDBURL = `mongodb+srv://${username}:${password}@pm-liccb.mongodb.net/test?retryWrites=true`;
-
-// const uri = "mongodb+srv://${username}:${password}@pm-liccb.mongodb.net/test?retryWrites=true";
 module.exports = function() {
   const db = config.get('liveDB');
-  // const db = LiveDBURL;
-
   mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, })
     .then(() => winston.info(`Connected to ${db}...`));
 }
