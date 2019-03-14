69106f03243fb098983c6f9d5f6929a78d20dfd0
tomavic <hbasheer@live.com>
2019-03-14 15:11:37 +0200

    newss

--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,14 @@
 const winston = require('winston');
 const mongoose = require('mongoose');
 const config = require('config');
+const username = 'tomas';
+const password = 'tomas%40admin19';
 
+const LiveDBURL = `mongodb+srv://${username}:${password}@cluster0-liccb.mongodb.net/test?retryWrites=true`
 module.exports = function() {
-  const db = config.get('db');
+  // const db = config.get('db');
+  const db = LiveDBURL;
+
   mongoose.connect(db, { useNewUrlParser: true, useCreateIndex: true, })
     .then(() => winston.info(`Connected to ${db}...`));
 }
